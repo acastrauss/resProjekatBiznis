1aef38f3c1010cbe2d6b41494b759d600612870a
Aleksandar Stamenković <stamenkovic.pr34.2018@uns.ac.rs>
2021-09-04 01:36:23 +0200

    FRONTEND IN PROCESS

---
+++ WebApplication1/Scripts/Skripte/Prikaz.js
@@ -0,0 +1,66 @@
+﻿$(document).ready(function () {
+    $('#drzavaNazivCB').change(function () {
+        if ($('#drzavaNazivCB').is(':checked')){
+            $('.nazivShow').show();
+        }
+        else {
+            $('.nazivShow').hide();
+        }
+    });
+
+    $('#datumUTCCB').change(function () {
+        if ($('#datumUTCCB').is(':checked')){
+            $('.dateUTCShow').show();
+        }
+        else {
+            $('.dateUTCShow').hide();
+        }
+    });
+
+    $('#kolicinaCB').change(function () {
+        if ($('#kolicinaCB').is(':checked')){
+            $('.kolicinaShow').show();
+        }
+        else {
+            $('.kolicinaShow').hide();
+        }
+    });
+
+    $('#temperaturaCB').change(function () {
+        if ($('#temperaturaCB').is(':checked')){
+            $('.temperaturaShow').show();
+        }
+        else {
+            $('.temperaturaShow').hide();
+        }
+    });
+
+    $('#pritisakCB').change(function () {
+        if ($('#pritisakCB').is(':checked')){
+            $('.pritisakShow').show();
+        }
+        else {
+            $('.pritisakShow').hide();
+        }
+    });
+
+    $('#vlaznostCB').change(function () {
+        if ($('#vlaznostCB').is(':checked')){
+            $('.vlaznostShow').show();
+        }
+        else {
+            $('.vlaznostShow').hide();
+        }
+    });
+
+    $('#vetarCB').change(function () {
+        if ($('#vetarCB').is(':checked')){
+            $('.vetarShow').show();
+        }
+        else {
+            $('.vetarShow').hide();
+        }
+    });
+
+
+});
